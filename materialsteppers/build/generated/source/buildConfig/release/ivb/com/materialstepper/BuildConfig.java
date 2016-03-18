d05c4948960eeb08b9bece8215fd54367cd01ab7
Yes Adil <yes.adilov@gmail.com>
2016-03-18 17:18:47 +0600

    EPTA

---
+++ materialsteppers/build/generated/source/buildConfig/release/ivb/com/materialstepper/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ivb.com.materialstepper;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ivb.com.materialstepper";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
