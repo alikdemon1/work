6b5a4af515993f7689edf9e40b5648538605a455
Yes Adil <yes.adilov@gmail.com>
2016-03-19 20:26:27 +0600

    testdone

---
+++ app/src/main/java/com/alisher/work/forTest/WelcomeTestActivity.java
@@ -0,0 +1,86 @@
+package com.alisher.work.forTest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.alisher.work.R;
+import com.alisher.work.adapters.RecyclerItemClickListener;
+import com.alisher.work.models.Category;
+import com.alisher.work.newtask.CategoryAdapter;
+import com.parse.FindCallback;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WelcomeTestActivity extends Activity {
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
+    private ArrayList<Category> categories;
+    private Bitmap bmp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_test);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.test_recycler_view);
+        mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mAdapter = new CategoryAdapter(getApplicationContext());
+        initializeData();
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Category catItem = categories.get(position);
+                Intent i = new Intent(WelcomeTestActivity.this, QuizActivity.class);
+                i.putExtra("catId", catItem.getId());
+                startActivity(i);
+            }
+        }));
+
+
+    }
+
+
+    public void initializeData(){
+        categories = new ArrayList<>();
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Category");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, com.parse.ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < list.size(); i++) {
+                        final Category catItem = new Category();
+                        ParseObject o = list.get(i);
+                        catItem.setId(o.getObjectId());
+                        catItem.setName(o.getString("name"));
+                        ParseFile image = (ParseFile) o.get("img");
+                        try {
+                            bmp = BitmapFactory.decodeByteArray(image.getData(), 0, image.getData().length);
+                            catItem.setImage(bmp);
+                        } catch (com.parse.ParseException e1) {
+                            e1.printStackTrace();
+                        }
+                        categories.add(catItem);
+                    }
+                    ((CategoryAdapter) mAdapter).setCategories(categories);
+                } else {
+                    Toast.makeText(WelcomeTestActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        ((CategoryAdapter)mAdapter).setCategories(categories);
+    }
+}
